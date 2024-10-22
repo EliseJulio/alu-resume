7567761211434ce4251f2e86a1e16b043a14cd2d
EliseJulio <j.hakiziman1@alustudent.com>
2024-10-22 23:42:47 +0200

    Julio Resume Website

---
+++ script.js
@@ -0,0 +1,48 @@
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.getElementById('navbar');
+
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+    
+    if (navbar.classList.contains('active')) {
+        menuIcon.src = './Images/x-square-solid-24.png'; 
+    } else {
+        menuIcon.src = './Images/menu-regular-24 (1).png'; 
+    }
+});
+
+const navLinks = document.querySelectorAll('.navbar a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navbar.classList.remove('active');
+        menuIcon.src = './Images/menu-regular-24 (1).png'; 
+    });
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
+const backToTopButton = document.getElementById('backToTop');
+window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+backToTopButton.addEventListener('click', function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+
